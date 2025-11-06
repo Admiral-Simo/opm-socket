358f228fe880f3ed8ec2ee6c3d9c5421a6096d4a
Admiral-Simo <admiralsimo16@gmail.com>
2025-11-06 12:37:43 +0100

    backend: initialize the spring boot backend with dependencies

---
+++ backend/src/test/java/com/network/opmsocket/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.opmsocket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
