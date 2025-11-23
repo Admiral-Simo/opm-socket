23ff8a55646cb799d66e0a440754171b27f2534b
Admiral-Simo <admiralsimo16@gmail.com>
2025-11-23 19:32:06 +0100

    backend: user service sync keycloak user with appuser

--- backend/src/main/java/com/network/opmsocket/backend/user/service/UserService.java
+++ backend/src/main/java/com/network/opmsocket/backend/user/service/UserService.java
@@ -1,4 +1,29 @@
 package com.network.opmsocket.backend.user.service;
 
+import com.network.opmsocket.backend.chat.repository.AppUserRepository;
+import com.network.opmsocket.backend.user.model.AppUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final AppUserRepository appUserRepository;
+    public AppUser syncUser(Jwt jwt) {
+        String userId = jwt.getSubject();
+        String username = jwt.getClaimAsString("preferred_username");
+        String email = jwt.getClaimAsString("email");
+
+
+        return appUserRepository.findById(userId)
+                .map(existingUser -> {
+                    existingUser.setUsername(username);
+                    existingUser.setEmail(email);
+                    return appUserRepository.save(existingUser);
+                })
+                .orElseGet(() -> {
+                    return appUserRepository.save(new AppUser(userId, username, email));
+                });
+    }
 }
