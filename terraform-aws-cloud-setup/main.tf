20c8b6d269bd87d5d8385965698ff6d155901668
Admiral-Simo <admiralsimo16@gmail.com>
2026-01-17 19:24:00 +0100

    terraform cognito configuration

--- terraform-aws-cloud-setup/main.tf
+++ terraform-aws-cloud-setup/main.tf
@@ -74,12 +74,20 @@ resource "aws_cognito_user_pool_client" "client" {
   user_pool_id = aws_cognito_user_pool.main.id
   generate_secret = true
 
+  supported_identity_providers = ["COGNITO"]
+
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
 
-  callback_urls = ["http://localhost:3000/api/auth/callback/cognito"]
-  logout_urls   = ["http://localhost:3000"]
+  callback_urls = [
+    "http://localhost:3000/api/auth/callback/cognito",
+    "http://127.0.0.1:3000/api/auth/callback/cognito"
+  ]
+
+  logout_urls   = ["http://localhost:3000", "http://127.0.0.1:3000"]
+
+  default_redirect_uri = "http://localhost:3000/api/auth/callback/cognito"
 }
 
 resource "aws_cognito_user_pool_domain" "main" {
